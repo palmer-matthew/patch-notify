89a165460362cc29059f98b0a0b2e8a0af123fd8
Matthew Palmer <palmer.matthew4567@gmail.com>
2023-05-06 23:21:50 -0500

    Add REST API calls to patch-notify.py
    Implemented Changes:
    
    Create function initialize_context. It requires as an argument: path
    which is the path to the .env file or any file which supplies the ENV
    variables needed for program function. It returns the env as a
    dictionary if it is successful. If it is not or a empty dictionary is
    received, the program will print a messages and will end prematurely.
    
    Create function retrieve_hosts. It requires as an argument: context
    which represents the ENV variables which are needed for functioning.
    The function makes a call to the REST API section which supplies the
    hosts information and returns the data as json if successful. If not, it
    will exit the program prematurely.
    
    Create function retrieve_hosts_collections. It requires as an argument:
    context which represents the ENV variables which are needed for
    functioning. The function makes a call to the REST API section which
    supplies the host_collection information and returns the data as json if
    successful. If not, it will exit the program prematurely. Once
    successful, it will make calls to REST API for each collection to get
    the detailed host collection information which includes the hosts id
    associated with the collection. Once returned, the information is
    appended to a list and a dictionary with this new list is returned. If
    it is unsuccesful at any moment, the program will end prematurely.
    
    Possible Changes:
    - Inline Documentation
    - Error Reporting

--- patch-notify.py
+++ patch-notify.py
@@ -1,2 +1,60 @@
+import sys
+from src.utils.api import get_data_basic_auth
+from src.utils.config import load_env_as_dict
+
+def initialize_context(path : str=None):
+    env = load_env_as_dict(path)
+    if env == {}:
+        print("Environment Variables file is empty or it cannot be located")
+        sys.exit()
+    return env
+
+def retrieve_hosts(context: dict={}):
+    hosts_api_url = context['BASE_API_URL'] + 'api/hosts?per_page=all'
+    host_data = get_data_basic_auth(hosts_api_url,context['USER'],context['PASSWD'])
+    if host_data == {}:
+        print("Unable to retrieve hosts information from API. Please try again next time")
+        sys.exit()
+    return host_data
+
+def retrieve_host_collections(context: dict={}):
+    host_coll_api_url = context['BASE_API_URL'] + '/katello/api/host_collections'
+    host_coll_data = get_data_basic_auth(host_coll_api_url,context['USER'],context['PASSWD'])
+    if host_coll_data == {}:
+        print("Unable to retrieve host collections information from API. Please try again next time")
+        sys.exit()
+    host_collections = []
+    for i in range(host_coll_data["total"]):
+        new_url = host_coll_api_url + f'/{i}'
+        host_coll = get_data_basic_auth(new_url,context['USER'],context['PASSWD'])
+        if host_coll == {}:
+            print("Unable to retrieve host collection information from API. Please try again next time")
+            sys.exit()
+        host_collections.append(host_coll)
+    if host_collections == []:
+        print("Unable to retrieve host collections information from API. Please try again next time")
+        sys.exit()
+    return { "host_collections": host_collections  }
+
+def display_help():
+    pass
+
+
 if __name__ == "__main__":
-    print("Patch-Notify Version: 1.0.2")
+    if len(sys.argv) == 1:
+        display_help()
+        sys.exit()
+    else:
+        for index, arg in enumerate(sys.argv[1:]):
+            if arg == "-e":
+                path = sys.argv[index + 2]
+        if path is None:
+            print("Please provide the path to the environment variables file")
+            sys.exit()
+        else:
+            context = initialize_context(path)
+            hosts = retrieve_hosts(context)
+            host_collections = retrieve_host_collections(context)
+            
+        
+
