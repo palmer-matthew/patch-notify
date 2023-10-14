3148b41b860e13aad3027b6616d9c51752379028
Matthew Palmer <palmer.matthew4567@gmail.com>
2023-10-14 16:29:12 -0500

    Create CI/CD Action

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
  
 setup(
     name="patch-notify",
-    version="1.0.0",
     author="Matthew Palmer",
     description="Python Executable which performs Email Notifications From Foreman",
     packages=find_packages()
