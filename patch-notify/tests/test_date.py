fc70d88f638c50ffb1cb9972e3e84b35ed15d506
Matthew Palmer <palmer.matthew4567@gmail.com>
2023-11-20 21:34:52 -0500

    Add Test Case to date Test Suite

--- patch-notify/tests/test_date.py
+++ patch-notify/tests/test_date.py
@@ -2,27 +2,48 @@ import unittest, calendar
 from datetime import date
 from src.utils.date import find_patch_dates
 
-class TestNovemberSchedule(unittest.TestCase):
+class TestNovSchedule(unittest.TestCase):
     def testSecondThu(self):
         dates = find_patch_dates(2023,11)
         self.assertEqual(dates['2nd Thu'], date(2023,11,16), 'Found Date Should be Equal to November 16, 2023')
     
     def testThirdThu(self):
         dates = find_patch_dates(2023,11)
-        self.assertEqual(dates['3rd Thu'], date(2023,11,23), 'Found Date Should be Equal to November 16, 2023')
+        self.assertEqual(dates['3rd Thu'], date(2023,11,23), 'Found Date Should be Equal to November 23, 2023')
 
     def testFourthThu(self):
         dates = find_patch_dates(2023,11)
-        self.assertEqual(dates['4th Thu'], date(2023,11,30), 'Found Date Should be Equal to November 16, 2023')
+        self.assertEqual(dates['4th Thu'], date(2023,11,30), 'Found Date Should be Equal to November 30, 2023')
     
     def testThirdTue(self):
         dates = find_patch_dates(2023,11)
-        self.assertEqual(dates['3rd Tue'], date(2023,11,21), 'Found Date Should be Equal to November 16, 2023')
+        self.assertEqual(dates['3rd Tue'], date(2023,11,21), 'Found Date Should be Equal to November 21, 2023')
 
     def testFourthTue(self):
         dates = find_patch_dates(2023,11)
-        self.assertEqual(dates['4th Tue'], date(2023,11,28), 'Found Date Should be Equal to November 16, 2023')
+        self.assertEqual(dates['4th Tue'], date(2023,11,28), 'Found Date Should be Equal to November 28, 2023')
 
+
+class TestOctSchedule(unittest.TestCase):
+    def testSecondThu(self):
+        dates = find_patch_dates(2023,10)
+        self.assertEqual(dates['2nd Thu'], date(2023,10,12), 'Found Date Should be Equal to October 12, 2023')
+    
+    def testThirdThu(self):
+        dates = find_patch_dates(2023,10)
+        self.assertEqual(dates['3rd Thu'], date(2023,10,19), 'Found Date Should be Equal to October 19, 2023')
+
+    def testFourthThu(self):
+        dates = find_patch_dates(2023,10)
+        self.assertEqual(dates['4th Thu'], date(2023,10,26), 'Found Date Should be Equal to October 26, 2023')
+    
+    def testThirdTue(self):
+        dates = find_patch_dates(2023,10)
+        self.assertEqual(dates['3rd Tue'], date(2023,10,17), 'Found Date Should be Equal to October 17, 2023')
+
+    def testFourthTue(self):
+        dates = find_patch_dates(2023,10)
+        self.assertEqual(dates['4th Tue'], date(2023,10,24), 'Found Date Should be Equal to October 24, 2023')
 
 if __name__ == '__main__':
     unittest.main()
