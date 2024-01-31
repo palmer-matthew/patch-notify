d3a552dc389a448145454831e9210a853959b546
Matthew Palmer <palmer.matthew4567@gmail.com>
2024-01-31 16:06:46 -0500

    fix: update date module II

--- patch-notify/src/utils/date.py
+++ patch-notify/src/utils/date.py
@@ -1,53 +1,68 @@
 import calendar, json
-from datetime import datetime
+from datetime import datetime, date
 
 def find_patch_dates(year:int = None, month:int = None):
+    """"""
+    # Check if a year and month were supplied to the function, if not, use the current month and year
     if year is None:
-        year = datetime.today().year
+        year = date.today().year
     if month is None:
-        month = datetime.today().month
+        month = date.today().month
 
-    calendarInstance = calendar.Calendar()
-    calendarMonth = calendarInstance.monthdatescalendar(year,month)
-    patchDatesForMonth = {}
+    calendar_instance = calendar.Calendar()
+    calendar_month = calendar_instance.monthdatescalendar(year, month)
+    patch_schedule = {}
 
-    firstDayInFirstWeek = calendarMonth[0][0]
+    first_week = calendar_month[0]
+    first_day = first_week[0]
+    exceptions = [calendar.MONDAY, calendar.SUNDAY]
     
-    if firstDayInFirstWeek.weekday() != calendar.MONDAY or firstDayInFirstWeek.weekday() != calendar.SUNDAY:
-        calendarMonth.pop(0)
+    # Remove the first calendar week if it does not begin on Sunday or Monday
+    if first_day.month == month:
+        if first_day.weekday() not in exceptions:
+            calendar_month.pop(0)
+    else:
+        calendar_month.pop(0)
     
-    tuesdayPositionInMonth = 0
-    thursdayPositionInMonth = 0
+    # variables to record the position of the weekdays in a specific week for the month 
+    mon_ordinal = 0
+    tue_ordinal = 0
+    thu_ordinal = 0
     
-    for week in calendarMonth:
+    for week in calendar_month:
         for day in week:
-            if day.month == month:
-                    if day.weekday() == calendar.TUESDAY:
-                        tuesdayPositionInMonth += 1
-                        if tuesdayPositionInMonth == 3:
-                            patchDatesForMonth["3rd Tue"] = day
-                        elif tuesdayPositionInMonth == 4:
-                            patchDatesForMonth["4th Tue"] = day
-                    if day.weekday() == calendar.THURSDAY:
-                        thursdayPositionInMonth += 1
-                        if thursdayPositionInMonth == 2:
-                            patchDatesForMonth["2nd Thu"] = day
-                        elif thursdayPositionInMonth == 3:
-                            patchDatesForMonth["3rd Thu"] = day
-                        elif thursdayPositionInMonth == 4:
-                            patchDatesForMonth["4th Thu"] = day
-    patchDatesForMonth['TBD'] = 'TBD'
-    patchDatesForMonth['Excluded'] = 'Excluded'
-    patchDatesForMonth['Decommissioned'] = 'Decomissioned' 
-    return patchDatesForMonth
+            if day.weekday() == calendar.MONDAY:
+                mon_ordinal += 1
+                if mon_ordinal == 1:
+                    patch_schedule['1st Mon'] = day
+            if day.weekday() == calendar.TUESDAY:
+                tue_ordinal += 1
+                if tue_ordinal == 2:
+                    patch_schedule['2nd Tue'] = day
+                elif tue_ordinal == 3:
+                    patch_schedule["3rd Tue"] = day
+                elif tue_ordinal == 4:
+                    patch_schedule["4th Tue"] = day
+            if day.weekday() == calendar.THURSDAY:
+                thu_ordinal += 1
+                if thu_ordinal == 2:
+                    patch_schedule["2nd Thu"] = day
+                elif thu_ordinal == 3:
+                    patch_schedule["3rd Thu"] = day
+                elif thu_ordinal == 4:
+                    patch_schedule["4th Thu"] = day
+    patch_schedule['TBD'] = 'TBD'
+    patch_schedule['Excluded'] = 'Excluded'
+    patch_schedule['Decommissioned'] = 'Decomissioned' 
+    return patch_schedule
 
 def parse_patch_dates(path: str):
     try:
         with open(path, 'r') as file:
             file_data = file.read()
             dates = json.loads(file_data)
-            for date in dates.keys():
-                dates[date] = datetime(dates[date][0], dates[date][1], dates[date][2])
+            for pdate in dates.keys():
+                dates[pdate] = date(dates[pdate][0], dates[pdate][1], dates[pdate][2])
     except:
         dates = {}
     dates['TBD'] = 'TBD'
